c8149cbd5fc407ab19e03b6a7192ae8840bbf6ec
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2025-11-22 19:59:29 -0300

    step: activity void

--- app/src/main/java/com/harukadev/studyinglifecycle/MainActivity.kt
+++ app/src/main/java/com/harukadev/studyinglifecycle/MainActivity.kt
@@ -28,49 +28,49 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            StudyingLifecycleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize()
-                            .background(MaterialTheme.colorScheme.background)
-                    ) {
-                        var name by remember { mutableStateOf("") }
-
-                        Text(
-                            text = "Hello!"
-                        )
-                        TextField(
-                            value = name,
-                            onValueChange = { name = it },
-                            label = { Text("Label") },
-                            singleLine = true
-                        )
-
-                        Spacer(Modifier.size(width = 0.dp, height = 25.dp))
-
-                        TextButton(onClick = {
-//                            val sendIntent = Intent().apply {
-//                                action = Intent.ACTION_SEND
-//                                type = "text/plain"
-//                                putExtra(Intent.EXTRA_TEXT, name)
-//                            }
-//                            startActivity(this@MainActivity, sendIntent, null)
-                            val intent =
-                                Intent(this@MainActivity, ReceiverActivity::class.java).apply {
-                                    putExtra("text", name)
-                                }
-
-                            startActivity(intent)
-
-                        }) {
-                            Text("Send Email")
-                        }
-                    }
-                }
-            }
-        }
+//        setContent {
+//            StudyingLifecycleTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Column(
+//                        modifier = Modifier
+//                            .padding(innerPadding)
+//                            .fillMaxSize()
+//                            .background(MaterialTheme.colorScheme.background)
+//                    ) {
+//                        var name by remember { mutableStateOf("Hello ") }
+//
+//                        Text(
+//                            text = "Hello!"
+//                        )
+//                        TextField(
+//                            value = name,
+//                            onValueChange = { name = it },
+//                            label = { Text("Label") },
+//                            singleLine = true
+//                        )
+//
+//                        Spacer(Modifier.size(width = 0.dp, height = 25.dp))
+//
+//                        TextButton(onClick = {
+////                            val sendIntent = Intent().apply {
+////                                action = Intent.ACTION_SEND
+////                                type = "text/plain"
+////                                putExtra(Intent.EXTRA_TEXT, name)
+////                            }
+////                            startActivity(this@MainActivity, sendIntent, null)
+//                            val intent =
+//                                Intent(this@MainActivity, ReceiverActivity::class.java).apply {
+//                                    putExtra("text", name)
+//                                }
+//
+//                            startActivity(intent)
+//
+//                        }) {
+//                            Text("Send Email")
+//                        }
+//                    }
+//                }
+//            }
+//        }
     }
 }
